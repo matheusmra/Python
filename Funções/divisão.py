3486f8f1b218cfa882b732cc78556715914b9dc7
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-11-15 21:33:28 -0300

    Create divisão.py

---
+++ Funções/divisão.py
@@ -0,0 +1,10 @@
+def div(valor1, valor2):
+    resp = valor1 / valor2
+    return resp
+
+print("Valor 1:")
+x = int(input())
+print("Valor 2:")
+y = int(input())
+resposta = div(x,y)
+print("Resultado da divisão:",resposta)
