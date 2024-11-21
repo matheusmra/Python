0b1b08c3a5e1744b8e05ccda68008618f9a54260
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-11-21 11:35:29 -0300

    Create soma.py

---
+++ Funções/soma.py
@@ -0,0 +1,9 @@
+def soma(valor1,valor2):
+    resp = valor1+valor2
+    return resp
+print("Valor1 =")
+x = int(input())
+print("Valor2 =")
+y = int(input())
+resposta = soma(x,y)
+print("Soma =", resposta)
